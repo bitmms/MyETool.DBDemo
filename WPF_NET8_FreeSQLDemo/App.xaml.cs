ee5697ee258198c5c833a0fb5c7b91ace0f87cdc
bitm <1759743261@qq.com>
2026-08-27 10:20:33 +0800

    feat: 基于 NET8 构建的 WPF 项目使用 FreeSQL 操作 MySQL、SQLite、PostgreSQL 数据库

---
+++ WPF_NET8_FreeSQLDemo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_NET8_FreeSQLDemo;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
